2544b703e836be2af15fd557c43520944095e504
MoSeale <57756845+MoSeale@users.noreply.github.com>
2021-03-08 00:31:50 -0500

    Update app.js
    add event listeners on click for filter and reset button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,6 +4,7 @@ var tableData = data;
 //Get a reference to the table body
 var tbody = d3.select("tbody");
 
+
 // //Console.log the UFO data from data.js
 
 // console.log(data);
@@ -36,7 +37,17 @@ data.forEach((UFOFinding) => {
 });
 
 
+//input data from page
+filter_button = d3.select("#filter-btn");
+reset_button = d3.select("#reset-btn");
+form = d3.select("form");
+
+
+
+
 // // Set up event handler function for the date input
+
+
 
 function filterUFOData(){
     
@@ -50,7 +61,9 @@ function filterUFOData(){
 
     //Filter Data by Date input by user
 
-    var filtered_sightings = tableData.filter(sightData => sightData.datetime === dateSelected);
+    var filtered_data = tableData.filter(sightData => sightData.datetime === dateSelected);
+
+    console.log(filtered_data);
 
     // reset the table initially displayed
 
@@ -58,30 +71,67 @@ function filterUFOData(){
 
     //repopulate table with filtered 
 
+    filtered_data.forEach((UFOFinding) => {
 
-    filtered_sightings.forEach((report) => {
-
-        // //Use d3 to append one table row `tr` for each  row object
+        // //Use d3 to append one table row `tr` for each  UFO data object
         var row = tbody.append("tr");
     
-        // //Use `Object.entries` to console.log each filtered data report value
-        Object.entries(report).forEach(([key, value]) => {
+        // //Use `Object.entries` to console.log each UFO data report value
+        Object.entries(UFOFinding).forEach(([key, value]) => {
     
             // Append a cell to the row for each value
     //      // in the  UFO data report object
-            var td = row.append("td");
+            var cell = row.append("td");
     
             //Use d3 to update each cell's text with
             // UFO report values (datetime, city, state, country, shape, durationMinutes, comments)
-            td.text(value);
+            cell.text(value);
         });
     });
+    
 
+
+};
+
+function resetUFOData(){
+    
+    // reset the table initially displayed
+
+    tbody.html("");
+
+    //repopulate table with filtered 
+
+    data.forEach((UFOFinding) => {
+
+        // //Use d3 to append one table row `tr` for each  UFO data object
+        var row = tbody.append("tr");
+    
+        // //Use `Object.entries` to console.log each UFO data report value
+        Object.entries(UFOFinding).forEach(([key, value]) => {
+    
+            // Append a cell to the row for each value
+    //      // in the  UFO data report object
+            var cell = row.append("td");
+    
+            //Use d3 to update each cell's text with
+            // UFO report values (datetime, city, state, country, shape, durationMinutes, comments)
+            cell.text(value);
+        });
+    });
+    
 
 
 };
 
 
+////Add event listener to call functions on click
+filter_button.on("click", filterUFOData);
+form.on("change", filterUFOData);
 
-////
-  
+
+//Reset data
+
+reset_button.on("click", resetUFOData);
+form.on("change", resetUFOData);
+
+  //
